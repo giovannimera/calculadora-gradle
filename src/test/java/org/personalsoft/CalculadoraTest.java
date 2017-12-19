46cc018b87868dd983a2439c7e300a69981a4156
Giovanni Mera <gmera@sura.com.co>
2017-12-19 13:52:18 -0500

    Se agrega prueba para la suma

---
+++ src/test/java/org/personalsoft/CalculadoraTest.java
@@ -0,0 +1,12 @@
+package org.personalsoft;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculadoraTest {
+	
+	@Test
+    public void testSuma() {
+        assertEquals(4, 2 + 2);
+    }
+}
