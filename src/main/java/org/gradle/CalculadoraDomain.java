369fc905fe47e2ca914f22ba422c9e6050bd4290
Giovanni Mera <gmera@sura.com.co>
2017-12-20 13:50:09 -0500

    Se crea logica para las operaciones de la calculadora

---
+++ src/main/java/org/gradle/CalculadoraDomain.java
@@ -0,0 +1,15 @@
+package org.gradle;
+
+public class CalculadoraDomain {
+
+	public int sumar(int a, int b){
+		int c = a + b;		
+		return c;
+	}
+	
+	public int restar(int a, int b){
+		int c = a - b;		
+		return c;
+	}
+	
+}
