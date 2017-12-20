35cfb2a11cdc26709e92d0bedba79aa700cce94f
giovannimera <giomera343@gmail.com>
2017-12-20 18:39:40 -0500

    Cambio hecho por Gabriel

--- src/main/java/org/gradle/CalculadoraDomain.java
+++ src/main/java/org/gradle/CalculadoraDomain.java
@@ -20,4 +20,15 @@ public class CalculadoraDomain {
 		}
 	}
 	
+	public void mySuperMethodThatDoesNotDoNothing(String superParameter){
+		
+		if("A".equals(superParameter)){
+			System.out.println("The value is A";
+		} else {
+			System.out.println("The value is: B";
+		}
+		
+		
+	}
+	
 }
