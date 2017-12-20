6283607d595ced26aa543d0961ee30c2420afe4d
alcuervo <alcuervo@personalsoft.com>
2017-12-20 18:42:35 -0500

    AlejoClass de Alejo

---
+++ src/main/java/org/gradle/AlejoClass.java
@@ -0,0 +1,13 @@
+package org.gradle;
+
+public class AlejoClass {
+	
+	public String sayMyName(String myName) {
+		return "me llamo "+myName;
+	}
+	
+	public Integer calculateBorn(int age,int year) {
+		return year-age;
+	}
+
+}
