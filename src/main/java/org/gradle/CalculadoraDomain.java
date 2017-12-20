bd17ee4ad66b0adceb7e3b60ebcc836be60c40b1
Giovanni Mera <gmera@sura.com.co>
2017-12-20 18:29:47 -0500

    Se implementa operación de division

--- src/main/java/org/gradle/CalculadoraDomain.java
+++ src/main/java/org/gradle/CalculadoraDomain.java
@@ -10,4 +10,14 @@ public class CalculadoraDomain {
 		return a - b;
 	}
 	
+	public int dividir(int a, int b){
+		try{
+			int c = a / b;
+			return c;
+		} catch(Exception e){
+			System.out.println(e.getMessage());
+			return -1;
+		}
+	}
+	
 }
