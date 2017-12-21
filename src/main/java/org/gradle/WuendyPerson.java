9af78bc247d2795b7558757e9e9d41541f8292e5
SURAMERICANA\wuenpede <wupertuz@personalsoft.comco>
2017-12-21 16:34:35 -0500

    Agrego clase WuendyPerson

---
+++ src/main/java/org/gradle/WuendyPerson.java
@@ -0,0 +1,28 @@
+package org.gradle;
+
+public class WuendyPerson {
+	private String nombres;
+	private String apellidos;
+	private int edad;
+	
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+}
