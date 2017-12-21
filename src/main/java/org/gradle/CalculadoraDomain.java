0fe8b94c0dfe44dc8254f80de5ef27b49e78fb43
giovannimera <giomera343@gmail.com>
2017-12-21 16:28:00 -0500

    clase creada por Gabriel

--- src/main/java/org/gradle/CalculadoraDomain.java
+++ src/main/java/org/gradle/CalculadoraDomain.java
@@ -1,5 +1,6 @@
 package org.gradle;
 
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -7,6 +8,31 @@ public class CalculadoraDomain {
 
 	private static final Logger log = Logger.getLogger(CalculadoraDomain.class.getName());
 
+	public CalculadoraDomain(){
+		ArrayList<PersonaGabriel> lista = crearPersonas();
+
+	}
+	
+	public ArrayList<PersonaGabriel> crearPersonas(){
+		ArrayList<PersonaGabriel> lista = new ArrayList<PersonaGabriel>();
+		PersonaGabriel gabriel = new PersonaGabriel();
+		gabriel.setNombre("Gabriel");
+		gabriel.setEdad(26);
+		gabriel.setProfesion("Ingeniero");
+		
+		lista.add(gabriel);
+		
+		PersonaGabriel daniela = new PersonaGabriel();
+		gabriel.setNombre("Daniela");
+		gabriel.setEdad(23);
+		gabriel.setProfesion("Administradora");
+		
+		lista.add(daniela);
+		return lista;
+	}
+	
+	
+	
 	public int sumar(int a, int b){
 		return a + b;
 	}
---
+++ src/main/java/org/gradle/PersonaGabriel.java
@@ -0,0 +1,28 @@
+package org.gradle;
+
+public class PersonaGabriel {
+	private String nombre;
+	private int edad;
+	private String profesion;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getProfesion() {
+		return profesion;
+	}
+	public void setProfesion(String profesion) {
+		this.profesion = profesion;
+	}
+	
+	
+}
